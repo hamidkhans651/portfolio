9b1c6229a6fc5e3b9be41af03be7a2e583b86a95
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2024-09-06 05:34:21 +0000

    add file from college pc

--- app/LandingPage.js
+++ app/LandingPage.js
@@ -1,14 +1,14 @@
-'use client'
-import styles from './page.module.scss'
-import { useState } from 'react';
-import { motion } from 'framer-motion';
-import useMousePosition from './utils/useMousePosition';
-import Header from '../app/components/header';
-import Menu from '../app/components/menu';
-import VerticalPixelTransition from '../app/components/pixelTransition/vertical';
+"use client";
+
+import styles from "./page.module.scss";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import useMousePosition from "./utils/useMousePosition";
+import Header from "./components/MenuBar/header";
+import Menu from "../app/components/menu";
+import VerticalPixelTransition from "./components/MenuBar/pixelTransition/vertical";
 
 export default function LandingPage() {
-
   const [isHovered, setIsHovered] = useState(false);
   const { x, y } = useMousePosition();
   const size = isHovered ? 400 : 40;
@@ -18,24 +18,35 @@ export default function LandingPage() {
     <main className={styles.main}>
       <Header menuIsActive={menuIsActive} setMenuIsActive={setMenuIsActive} />
       <Menu menuIsActive={menuIsActive} />
-      <VerticalPixelTransition menuIsActive={menuIsActive}/>
+      <VerticalPixelTransition menuIsActive={menuIsActive} />
       <motion.div
         className={styles.mask}
         animate={{
-          WebkitMaskPosition: `${x - (size / 2)}px ${y - (size / 2)}px`,
+          WebkitMaskPosition: `${x - size / 2}px ${y - size / 2}px`,
           WebkitMaskSize: `${size}px`,
         }}
         transition={{ type: "tween", ease: "backOut", duration: 0.5 }}
       >
-        <p onMouseEnter={() => { setIsHovered(true) }} onMouseLeave={() => { setIsHovered(false) }}>
-          A visual designer - with skills that haven&apos;t been replaced by A.I (yet) - making good shit only if the paycheck is equally good.
+        <p
+          onMouseEnter={() => {
+            setIsHovered(true);
+          }}
+          onMouseLeave={() => {
+            setIsHovered(false);
+          }}
+        >
+          A visual designer - with skills that haven&apos;t been replaced by A.I
+          (yet) - making good shit only if the paycheck is equally good.
         </p>
       </motion.div>
 
       <div className={styles.body}>
-        <p>I&apos;m a <span>selectively skilled</span> product designer with strong focus on producing high quality & impactful digital experience.</p>
+        <p>
+          I&apos;m a <span>Hamid skilled</span>Front End developer with strong
+          focus on cloud native technologies with high quality & impactful
+          digital experience products.
+        </p>
       </div>
-
     </main>
-  )
+  );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,34 +1,31 @@
-'use client'
+"use client";
 
 import { useEffect, useState, useRef } from "react";
-import { TimelineDemo } from './acenity.UI/UI/TimelineDemo'
+import { TimelineDemo } from "./acenity.UI/UI/TimelineDemo";
 import { useScroll, useTransform, motion } from "framer-motion";
-import Lenis from 'lenis';
-import LandingPage from '../app/LandingPage'
+import Lenis from "lenis";
+import LandingPage from "../app/LandingPage";
 
 export default function Home() {
-
   const container = useRef();
   const { scrollYProgress } = useScroll({
     target: container,
-    offset: ["start start", "end end"]
-  })
+    offset: ["start start", "end end"],
+  });
 
   useEffect(() => {
-    const lenis = new Lenis()
+    const lenis = new Lenis();
 
     function raf(time) {
-      lenis.raf(time)
-      requestAnimationFrame(raf)
+      lenis.raf(time);
+      requestAnimationFrame(raf);
     }
 
-    requestAnimationFrame(raf)
-  }, [])
-
+    requestAnimationFrame(raf);
+  }, []);
 
   return (
     <main ref={container} className="relative h-[200vh]">
-
       <Section1 scrollYProgress={scrollYProgress} />
       <Section2 scrollYProgress={scrollYProgress} />
     </main>
@@ -36,41 +33,28 @@ export default function Home() {
 }
 
 const Section1 = ({ scrollYProgress }) => {
-
   const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
-  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5])
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5]);
   const stickyElement = useRef(null);
   return (
-
-    <motion.div style={{ scale, rotate }} className="sticky top-0 h-screen bg-white  text-white ">
+    <motion.div
+      style={{ scale, rotate }}
+      className="sticky top-0 h-screen bg-white  text-white "
+    >
       <LandingPage />
 
-      <main className="lg:block md:block hidden sm:hidden xs:hidden">
-
-      </main>
+      <main className="lg:block md:block hidden sm:hidden xs:hidden"></main>
     </motion.div>
-  )
-}
+  );
+};
 
 const Section2 = ({ scrollYProgress }) => {
-
   const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const rotate = useTransform(scrollYProgress, [0, 1], [5, 0])
-
-
+  const rotate = useTransform(scrollYProgress, [0, 1], [5, 0]);
 
   return (
-
-
     <motion.div style={{ scale, rotate }} className="relative h-screen">
-
-
       <TimelineDemo />
     </motion.div>
-
-  )
-}
-
-
-
-
+  );
+};
