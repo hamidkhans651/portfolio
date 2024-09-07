b7c67cf9377c0e304f7f8cb34389b60ef7dd9832
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2024-09-07 04:16:20 -0700

    finalize the structure

--- app/components/MenuBar/header/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './style.module.scss';
-
-export default function Index({menuIsActive, setMenuIsActive}) {
-  return (
-    <div className={styles.header}>
-        <div onClick={() => {setMenuIsActive(!menuIsActive)}} className={`${styles.burger} ${menuIsActive ? styles.burgerActive : ""}`}>
-        </div>
-    </div>
-)}
--- app/page.jsx
+++ app/page.jsx
@@ -1,63 +1,27 @@
-"use client";
-
-import { useEffect, useState, useRef } from "react";
-import { TimelineDemo } from "./acenity.UI/UI/TimelineDemo";
-import { useScroll, useTransform, motion } from "framer-motion";
-import Lenis from "lenis";
 import LandingPage from "../app/LandingPage";
-import Hero from "../app/HeroPage/Hero"
+import FooterComp from './components/Footer/Footer'
+import Skill from './components/Skill';
+import { TimelineDemo } from "./acenity.UI/UI/TimelineDemo";
+
 
 
 export default function Home() {
-  const container = useRef();
-  const { scrollYProgress } = useScroll({
-    target: container,
-    offset: ["start start", "end end"],
-  });
-
-  useEffect(() => {
-    const lenis = new Lenis();
-
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
-  }, []);
-
   return (
-    <main >
+    <main>
+      <div className="lg:block md:block hidden sm:hidden xs:hidden">
+        < LandingPage />
+      </div>
+      <div>
+      <TimelineDemo />
+      </div>
 
-      <Section1 scrollYProgress={scrollYProgress} />
-      <Section2 scrollYProgress={scrollYProgress} />
-
+      <div id='Projects'>
+        <Skill />
+      </div>
+      <div id='Footer'>
+        <FooterComp />
+      </div>
 
     </main>
-  );
-}
-
-const Section1 = ({ scrollYProgress }) => {
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
-  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5]);
-  const stickyElement = useRef(null);
-  return (
-    <motion.div
-      style={{ scale, rotate }}
-      className="sticky top-0 h-screen bg-white  text-white ">
-      <LandingPage />
-      <main className="lg:block md:block hidden sm:hidden xs:hidden">
-      </main>
-    </motion.div>
-  );
-};
-
-const Section2 = ({ scrollYProgress }) => {
-  const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
-  const rotate = useTransform(scrollYProgress, [0, 1], [5, 0]);
-
-  return (
-    <motion.div style={{ scale, rotate }} className="relative h-screen">
-      <TimelineDemo />
-    </motion.div>
   );
 };
