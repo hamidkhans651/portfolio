e5beda1c31df7f60deb0d1bcad963ba6eb6627fd
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2024-09-07 13:46:54 -0700

    fix

--- components/Landing/index.jsx
+++ components/Landing/index.jsx
@@ -12,10 +12,10 @@ export default function Home() {
   const firstText = useRef(null);
   const secondText = useRef(null);
   const slider = useRef(null);
-  let xPercent = 0;
-  let direction = -1;
+  const xPercent = useRef(0);
+  const direction = useRef(-1);
 
-  useLayoutEffect( () => {
+  useLayoutEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
     gsap.to(slider.current, {
       scrollTrigger: {
@@ -23,25 +23,24 @@ export default function Home() {
         scrub: 0.25,
         start: 0,
         end: window.innerHeight,
-        onUpdate: e => direction = e.direction * -1
+        onUpdate: e => direction.current = e.direction * -1
       },
       x: "-500px",
-    })
+    });
     requestAnimationFrame(animate);
-  }, [])
+  }, []);
 
   const animate = () => {
-    if(xPercent < -100){
-      xPercent = 0;
+    if (xPercent.current < -100) {
+      xPercent.current = 0;
+    } else if (xPercent.current > 0) {
+      xPercent.current = -100;
     }
-    else if(xPercent > 0){
-      xPercent = -100;
-    }
-    gsap.set(firstText.current, {xPercent: xPercent})
-    gsap.set(secondText.current, {xPercent: xPercent})
+    gsap.set(firstText.current, { xPercent: xPercent.current });
+    gsap.set(secondText.current, { xPercent: xPercent.current });
     requestAnimationFrame(animate);
-    xPercent += 0.1 * direction;
-  }
+    xPercent.current += 0.1 * direction.current;
+  };
 
   return (
     <motion.main variants={slideUp} initial="initial" animate="enter" className={styles.landing}>
@@ -64,5 +63,5 @@ export default function Home() {
         <p>Designer & Developer</p>
       </div>
     </motion.main>
-  )
+  );
 }
