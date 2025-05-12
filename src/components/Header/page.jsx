9d958fff42552233a3e54cd2d47fce5a9fb92e5c
Muhammad Hamid <61172566+hamidkhans651@users.noreply.github.com>
2025-05-12 02:48:41 -0700

    update

--- src/components/Header/page.jsx
+++ src/components/Header/page.jsx
@@ -59,8 +59,8 @@ export default function Index() {
                 <div className={styles.logo}>
                     <p className={styles.copyright}>©</p>
                     <div className={styles.name}>
-                        <p className={styles.codeBy}>Code by</p>
-                        <p className={styles.dennis}>Harry</p>
+                        <p className={styles.codeBy}>code by</p>
+                        <p className={styles.dennis}>Hamid</p>
                         <p className={styles.snellenberg}>Devloper</p>
                     </div>
                 </div>
